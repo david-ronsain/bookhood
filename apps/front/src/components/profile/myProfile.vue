29be5126e19a5eef759ed6adc7dd9cdc0a366c1a
David Ronsain <davidronsain@gmail.com>
2024-03-17 11:08:37 +0100

    create component myProfile

---
+++ apps/front/src/components/profile/myProfile.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+	import { BhCard } from '@bookhood/ui'
+	import { IUser } from '@bookhood/shared'
+	import { computed, onMounted, onUnmounted } from 'vue'
+	import { useProfileStore } from '../../store'
+	import {
+		mdiBookArrowLeftOutline,
+		mdiBookArrowRightOutline,
+		mdiBookOutline,
+		mdiGiftOutline,
+		mdiMapMarkerOutline,
+		mdiTimerSand,
+	} from '@mdi/js'
+
+	interface Props {
+		profile: IUser
+	}
+
+	const profileStore = useProfileStore()
+	const props = defineProps<Props>()
+
+	const stats = computed(() => profileStore.stats)
+
+	onMounted(() => {
+		if (props.profile) {
+			profileStore.loadProfileStats()
+		}
+	})
+
+	onUnmounted(() => {
+		profileStore.$patch({ stats: null })
+	})
+</script>
+
+<template>
+	<bh-card
+		class="profile-stats"
+		v-if="stats"
+		:hover="false"
+		:title="profile.firstName + ' ' + profile.lastName">
+		<v-row>
+			<v-col
+				cols="12"
+				md="6">
+				<v-icon
+					size="20"
+					color="primary"
+					>{{ mdiBookOutline }}</v-icon
+				>{{ $t('account.myProfile.nbBooks', { nb: stats.nbBooks }) }}
+			</v-col>
+			<v-col
+				cols="12"
+				md="6">
+				<v-icon
+					size="20"
+					color="primary"
+					>{{ mdiMapMarkerOutline }}</v-icon
+				>{{ $t('account.myProfile.nbPlaces', { nb: stats.nbPlaces }) }}
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col
+				cols="12"
+				md="6">
+				<v-icon
+					size="20"
+					color="primary"
+					>{{ mdiGiftOutline }}</v-icon
+				>{{
+					$t('account.myProfile.nbBooksToGive', {
+						nb: stats.nbBooksToGive,
+					})
+				}}
+			</v-col>
+			<v-col
+				cols="12"
+				md="6">
+				<v-icon
+					size="20"
+					color="primary"
+					>{{ mdiTimerSand }}</v-icon
+				>{{
+					$t('account.myProfile.nbBooksToLend', {
+						nb: stats.nbBooksToLend,
+					})
+				}}
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col
+				cols="12"
+				md="6">
+				<v-icon
+					size="20"
+					color="primary"
+					>{{ mdiBookArrowLeftOutline }}</v-icon
+				>{{
+					$t('account.myProfile.nbIncomingRequests', {
+						nb: stats.nbIncomingRequests,
+					})
+				}}
+			</v-col>
+			<v-col
+				cols="12"
+				md="6">
+				<v-icon
+					size="20"
+					color="primary"
+					>{{ mdiBookArrowRightOutline }}</v-icon
+				>{{
+					$t('account.myProfile.nbOutgoingRequests', {
+						nb: stats.nbOutgoingRequests,
+					})
+				}}
+			</v-col>
+		</v-row>
+	</bh-card>
+</template>
---
+++ apps/front/tests/components/profile/myProfile.test.ts
@@ -0,0 +1,59 @@
+import { VueWrapper, config, mount } from '@vue/test-utils'
+import { vi } from 'vitest'
+import ProfileCard from '../../../src/components/profile/profileCard.vue'
+import vuetify from '../../../src/plugins/vuetify'
+import { createTestingPinia } from '@pinia/testing'
+import { useProfileStore } from '../../../src/store'
+import { externalProfile, myProfile, userStats } from '../../data/profileData'
+import MyProfile from '../../../src/components/profile/myProfile.vue'
+
+vi.mock('vue-i18n', () => ({
+	useI18n: () => ({
+		t: (key: string) => key,
+		d: (key: string) => key,
+	}),
+}))
+
+config.global.mocks = {
+	$t: (tKey) => tKey,
+}
+
+global.ResizeObserver = require('resize-observer-polyfill')
+
+describe('Testing the component MyProfile', () => {
+	let wrapper: VueWrapper
+	let profileStore
+
+	beforeEach(() => {
+		vi.clearAllMocks()
+		wrapper = mount(MyProfile, {
+			props: {
+				profile: myProfile,
+			},
+			global: {
+				plugins: [
+					vuetify,
+					createTestingPinia({
+						createSpy: vi.fn,
+					}),
+				],
+			},
+		})
+
+		profileStore = useProfileStore()
+	})
+
+	it('should mount the component', () => {
+		expect(wrapper.findAll('.profile-stats')).toMatchObject([])
+		expect(wrapper.findComponent(MyProfile).exists()).toBe(true)
+	})
+
+	it('should mount the component with stats', async () => {
+		profileStore.stats = userStats
+		await wrapper.vm.$nextTick()
+
+		expect(wrapper.findComponent(MyProfile).text()).toContain(
+			'account.myProfile.nbBooks',
+		)
+	})
+})
